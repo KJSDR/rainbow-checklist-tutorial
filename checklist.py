c9b0956a8d99efd39fabefa18cf34088369c4185
Killian <killian.delamarre-ruer@students.dominican.edu>
2024-09-24 22:19:21 -0700

    created helper functions

--- checklist.py
+++ checklist.py
@@ -7,15 +7,52 @@ def create(item):
 def read(index):
     return checklist[index]
 
-
-
 def update(index, item):
     checklist[index] = item
 
 def destroy(index):
     checklist.pop(index)
 
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print(str(index) + list_item)
+        index += 1
+
+def mark_completed(index):
+    if index < len(checklist):
+        checklist[index] = "√ " + checklist[index]
+
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+
+    # Read item
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+
+        # Remember that item_index must actually exist or our program will crash.
+        read(item_index)
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+
+    # Catch all
+    else:
+        print("Unknown Option")
+
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
+
+
 def test():
+    
     create("purple sox")
     create("red cloak")
 
@@ -26,7 +63,12 @@ def test():
     destroy(1)
 
     print(read(0))
-    print(read(1))
+    
+    select("C")
+    list_all_items()
+
+    select("R")
+    llist_all_items()
 
 test()
 
